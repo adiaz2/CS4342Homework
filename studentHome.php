0cbc042d30ca280f4245e4f4687c74708caba388
Alejandro Diaz <alejandrodiaz@Alejandros-MacBook-Pro.local>
2018-11-13 10:01:07 -0700

    Created new requests forms and php code to communicate with the databases

---
+++ studentHome.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<?php if (!isset($_COOKIE['loggedin'])) die("You are not logged in!"); ?>
+<html>
+
+<head>
+</head>
+
+<body>
+<h1>CS4342 Database Management Electronic Access - Web Interface</h1>
+<h2>PHP demos </h2>
+<p>Welcome to the Electronic Access Web Interface</p>
+<br/>
+<h2>Main Menu</h2>
+<ul>
+    <li><a href="studentForm.html">Fill Out New Student Form</a></li>
+    <li><a href="requestForm.html">Fill Out New Access Request Form</a></li>
+    <li><a href="logout.php">Logout</a></li>
+</ul>
+</body>
+
+</html>
